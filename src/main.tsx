0c5f4bd99119ed425929164e74c08910f2e0b896
Priyanshukj02 <priyanshukj02@gmail.com>
2023-04-15 08:34:38 +0530

    root: div -> body

--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,15 @@ import { Contact } from "./components/Contact";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <div className="w-full h-auto box-border bg-white font-poppins">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
-  </div>
+ReactDOM.createRoot(document.getElementById("body") as HTMLElement).render(
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+  </Router>
 );
