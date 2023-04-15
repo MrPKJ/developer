6212f4a05ad29341d45f7740afd9c405e3f51bb0
Priyanshukj02 <priyanshukj02@gmail.com>
2023-04-15 20:48:07 +0530

    ...

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,15 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="w-full h-screen bg-[#f9f9f9] flex flex-row justify-center items-center">
+      <section className="w-1/2">
+        <article className="flex flex-col justify-center items-left w-2/4 text-black font-poppins">
+          <h1 className="font-bold text-5xl">Front-End React Developer</h1>
+          <p className="font-mulish text-[#767676]">
+            Hi, I'm Priyanshu Kumar Jha. A passionate Programmer based in Bihar,
+            India. 📍
+          </p>
+        </article>
+      </section>
+    </main>
+  );
 };
