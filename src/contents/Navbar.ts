b0200ed20c8b51148b928f20de528d4d74f2ba83
Priyanshukj02 <priyanshukj02@gmail.com>
2023-04-15 07:50:06 +0530

    Nav-contents

---
+++ src/contents/Navbar.ts
@@ -0,0 +1,22 @@
+export const nav: { id: string; title: string; path: string }[] = [
+  {
+    id: "home",
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: "about",
+    title: "About",
+    path: "/about",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    path: "/contact",
+  },
+];
