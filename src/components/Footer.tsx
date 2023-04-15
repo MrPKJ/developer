fa369bc875e66230fdb99ddc6803658624b416ed
Priyanshukj02 <priyanshukj02@gmail.com>
2023-04-15 14:04:43 +0530

    responsive bug fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   const Year = new Date().getFullYear();
   return (
     <footer className="w-full h-32 bg-[#2d2e32] font-poppins box-border text-white flex flex-col xl:justify-between justify-around items-center xl:px-96 px-10 xl:py-10 py-5 xl:flex-row">
-      <h3 className="font-bold text-[1rem]">
+      <h3 className="font-bold text-[1rem] text-center">
         Copyright © <span>{Year}</span>. All rights are reserved
       </h3>
       <div className="flex flex-row justify-end items-center gap-4 cursor-pointer">
